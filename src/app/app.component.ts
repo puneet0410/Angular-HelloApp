20babb9f46ae62bc16b1f59a3e79b971c83c5862
puneet0410 <puneetp2200@gmail.com>
2025-03-24 23:53:26 +0530

    UC4_Two-Way-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Helloworld';
   imgUrl = "../assets/BL_logo_square_jpg.jpg";
-  url = "https://www.bridgelabz.com";  
+  url = "https://www.bridgelabz.com";
+  userName: string = ""; 
+  nameError: string = "";  
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
@@ -18,4 +20,19 @@ export class AppComponent implements OnInit {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+
+  onInput($event: Event) {
+    console.log("Change Event Occurred!", ($event.target as HTMLInputElement).value);
+  
+    this.userName = ($event.target as HTMLInputElement).value;  
+    
+    const nameRegex = /^[A-Z]{1}[a-zA-Z\s]{2,}$/;  
+  
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
+  
 }
